921a735bc4b39e4d9dca69c9756c2dc413bbc1ff
Elkhan <is.elxan@gmail.com>
2020-05-25 08:57:02 +0400

    fix custom pagination

--- client-side/src/Components/Common/Pagination/index.jsx
+++ client-side/src/Components/Common/Pagination/index.jsx
@@ -10,7 +10,6 @@ const Pagination = ({coinsPerPage, totalCoins, changeCurrPage}) => {
         pages.push(i);
     }
 
-
     return (
         <div className="pagination">
             <ul>
@@ -31,32 +30,3 @@ const Pagination = ({coinsPerPage, totalCoins, changeCurrPage}) => {
 
 
 export default Pagination;
-
-
-/*
-
-import React from "react";
-import style from "./styles/Pagination.module.css";
-
-const Pagination = ({coinsPerPage, totalCoins, paginate}) => {
-    const pageNumbers = [];
-
-    for (let i = 1; i <= Math.ceil(totalCoins / coinsPerPage); i++) {
-        pageNumbers.push(i);
-    }
-
-    return totalCoins ? (
-        <div>
-            <ul className={style.main}>
-                {pageNumbers.map(number => (
-                    <li key={number} onClick={() => paginate(number)}>
-                        <p>{number}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    ) : '';
-}
-
-export default Pagination;
-*/
